67d644c573cee650fade9e2a36c9d75d3393445b
Roman Abrahamson <rrcabrahamson@gmail.com>
2025-01-09 15:22:32 -0800

    Create Empty Elevator Class

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+    private TalonFX left, right;
+    public Elevator() {
+
+    }
+
+    
+}
