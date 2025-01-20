1479fcbbf2b26d8bce87f470cff6ee39d14db2f4
BreakerBots <132958980+BreakerBots-public@users.noreply.github.com>
2025-01-20 15:26:45 -0800

    fix build

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.BreakerLib.util.logging.BreakerLog;
 import frc.robot.Constants.IndexerConstants;
@@ -21,7 +22,7 @@ public class Indexer extends SubsystemBase {
     }
 
     public Command setState(IndexerState state) {
-        return runOnce(() -> set(state), this);
+        return Commands.runOnce(() -> set(state), this);
     }
 
     public static enum IndexerState {
