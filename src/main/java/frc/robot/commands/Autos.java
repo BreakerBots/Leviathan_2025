18b0f7027103d31a865eb489d4965fbd7e882fb6
BreakerBots <132958980+BreakerBots-public@users.noreply.github.com>
2025-02-22 17:53:58 -0800

    auto tweaks and more paths WIP
    Co-Authored-By: CyberBladeX <maxziyangxu@gmail.com>
    Co-Authored-By: Rev <53663385+reverset@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,8 +5,6 @@
 package frc.robot.commands;
 
 import choreo.auto.AutoFactory;
-import choreo.auto.AutoRoutine;
-import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.ReefPosition;
@@ -18,6 +16,12 @@ import frc.robot.subsystems.superstructure.Superstructure;
 public class Autos {
     private final Superstructure superstructure;
     private final AutoFactory autoFactory;
+
+    public static enum StartPosition {
+        ONE,
+        TWO,
+        THREE,
+    }
 
     public Autos(Superstructure superstructure) {
         this.superstructure = superstructure;
@@ -45,10 +49,20 @@ public class Autos {
         return routine.cmd();
     }
 
-    public Command start1Full() {
+    /**
+     * Given a starting point, it will then go to
+     * Reef J -> K -> L -> A, each time it will also visit the Coral Player Station
+     * to restock on Coral.
+     */
+    public Command startAnywhereThenJKLA(StartPosition startPos) {
         final var routine = autoFactory.newRoutine("Start1Full");
 
-        final var startToReefJ = routine.trajectory("Start 1 to Reef J");
+        final var startToReefJ = switch (startPos) {
+            case ONE -> routine.trajectory("Start 1 to Reef J");
+            case TWO -> routine.trajectory("Start 2 to Reef J");
+            case THREE -> routine.trajectory("Start 3 to Reef J");
+        };
+
         final var reefJToCoralPS = routine.trajectory("Reef J to Coral PS");
         final var coralPSToReefK = routine.trajectory("Coral PS to Reef K");
         final var reefKToCoralPS = routine.trajectory("Reef K to Coral PS");
